01da2e3eea48f5cd0c6bab36c81872fa0828411e
Benjamin Zax <benzax@gmail.com>
2016-04-16 01:19:12 -0400

    Add play-new discard-old Player
    Add a Player who plays newest cards and discards oldest cards.  Whenever given
    a hint, this player will assume it means that the newest card in hand targeted
    by that hint is playable.  If not hinted in the past round, they will do a
    cursory check for other playable cards in hand.  They will only play these if
    they know color and number directly, and they are confused by rainbow cards.
    When giving hints, they assume that others follow the same rules, and starting
    with the following player, will find the first playable card which can be
    hinted without also hinting a newer card in the same hand.
    Otherwise, they discard the oldest card from their hand.

---
+++ newest_card_player.py
@@ -0,0 +1,100 @@
+"""A Player who prefers to play new cards and discard old cards
+
+Newest Card players prefer to play the newest hinted card each time they
+receive a hint.  Otherwise they will give hints about playable cards only
+if there is a hint for which the newest card affected by that hint is
+playable.  When discarding, they will discard their oldest card.
+Possible improvements include hinting fives for protection if they are
+about to be discarded, or playing all 1s from starting round hints.
+"""
+
+from hanabi_classes import *
+
+class NewestCardPlayer:
+    def get_my_playable(self, cards, progress, r):
+        playableCards = []
+        for card in cards:
+            suit = ''
+            value = ''
+            for info in card['direct']:
+                if info in r.suits:
+                    suit = info
+                elif info in '12345':
+                    value = info
+                #else something was invalid
+            if suit != '' and value != '' and progress[suit] == int(value) - 1:
+                playableCards.append(card)
+        return playableCards
+
+    def get_playable(self, cards, progress):
+        playableCards = []
+        for card in cards:
+            value, suit = card['name']
+            if progress[suit] == int(value) - 1:
+                playableCards.append(card)
+        return playableCards
+
+    # find the newest card in your hand for which info was relevant
+    # as long as you haven't drawn any new cards, this should have the same
+    # outcome as get_newest_hinted, but without looking at your cards
+    def get_my_newest_hinted(self, cards, info):
+        hinted = [card for card in cards if info in card['direct']]
+        if hinted:
+            return max(hinted, key=lambda card: card['time'])
+
+    # find the newest card in another player's hand which info targets 
+    def get_newest_hinted(self, cards, info):
+        hinted = [card for card in cards if info in card['name']
+                or (info in 'rygbw' and '?' in card['name'])]
+        if hinted:
+            return max(hinted, key=lambda card: card['time'])
+
+    def possible_hints(self, name):
+        if '?' not in name:
+            return name
+        else:
+            return name[0] + 'rygbw'
+
+    # pick a card to discard, let's go with the oldest
+    # smarter players might not discard known fives...
+    def get_discard(self, cards):
+        return min(cards, key=lambda card: card['time'])
+        
+
+    def play(self, r):
+        me = r.whoseTurn
+        cards = r.h[me].cards # don't look!
+        progress = r.progress
+        # first preference is to play hinted cards, then known
+        # was I hinted since my last turn?
+        # only care about the first hint received in that time
+        for playType, playValue in r.playHistory[-r.nPlayers:]:
+            if playType == 'hint':
+                target, info = playValue
+                if target == me:
+                    play = self.get_my_newest_hinted(cards, info)
+                    if play:
+                      return 'play', play
+
+        # check my knowledge about my cards, are any guaranteed playable?
+        myPlayableCards = self.get_my_playable(cards, progress, r)
+
+        if myPlayableCards != []:
+            return 'play', random.choice(myPlayableCards)
+ 
+        if r.hints > 0:
+            # look around at each other hand to see if anything is playable
+            for i in range(me+1, r.nPlayers) + range(0, me):
+                othersCards = r.h[i].cards
+                playableCards = self.get_playable(othersCards, progress)
+
+                if playableCards != []:
+                    for card in playableCards:
+                        # is there a hint for which this card is the newest?
+                        for info in self.possible_hints(card['name']):
+                            if card == self.get_newest_hinted(othersCards, info):
+                                return 'hint', (i, info)
+
+
+        # alright, don't know what to do, let's toss
+        return 'discard', self.get_discard(cards)
