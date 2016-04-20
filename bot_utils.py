a4fb645464a00ba24c969aeff6ca888fc280f229
Robert Kaspar <chikinn@gmail.com>
2016-04-19 18:14:55 -0700

    Fix #13

--- bot_utils.py
+++ bot_utils.py
@@ -21,12 +21,20 @@ def deduce_plays(cards, progress, suits):
             if info in suits:
                 if suit == '':
                     suit = info
-                elif suit != info:
+                    if '?' in suits:
+                        suit += '?' # With one hint, can't rule out rainbow.
+                elif suit not in info:
                     suit = '?' # Rainbow card matches more than one suit.
-            elif info in '123456789':
+            elif info in '123456789': # Maybe we'll add variants later.
                 value = info
+        
+        if len(suit) == 2: # Try to deduce that card is not rainbow.
+            for info in card['indirect']:
+                if info in suits:
+                    suit.replace('?', '')
+                    break # Already found the info we needed.
 
-        if suit != '' and value != '' and progress[suit] == int(value) - 1:
+        if len(suit) == 1 and value != '' and progress[suit] == int(value) - 1:
             plays.append(card)
 
     return plays
