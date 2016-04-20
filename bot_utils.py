92ae5b0eeae557072503f4d4cb5f7b13bb9f8386
ghutchings <ghutchings87@gmail.com>
2016-04-20 01:01:52 -0400

    Fix rainbow behavior in deduce_plays()
    `suit.replace('?', '')` does not change `suit`, so changed to `suit =
    suit.replace('?', '')`
    
    Also, corrected menu options in "human".

--- bot_utils.py
+++ bot_utils.py
@@ -16,7 +16,7 @@ def deduce_plays(cards, progress, suits):
     """Return a list of plays (subset of input); fine to call on own hand."""
     plays = []
     for card in cards:
-        suit, value = '', ''    
+        suit, value = '', ''
         for info in card['direct']:
             if info in suits:
                 if suit == '':
@@ -27,11 +27,11 @@ def deduce_plays(cards, progress, suits):
                     suit = '?' # Rainbow card matches more than one suit.
             elif info in '123456789': # Maybe we'll add variants later.
                 value = info
-        
+
         if len(suit) == 2: # Try to deduce that card is not rainbow.
             for info in card['indirect']:
                 if info in suits:
-                    suit.replace('?', '')
+                    suit = suit.replace('?', '')
                     break # Already found the info we needed.
 
         if len(suit) == 1 and value != '' and progress[suit] == int(value) - 1:
