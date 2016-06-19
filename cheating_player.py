fb760c988921b3b6185e949b60e218acf3342eb4
Robert Kaspar <chikinn@gmail.com>
2016-06-19 13:12:01 -0700

    Clean up & python3-compatibilize CheatingPlayer

--- cheating_player.py
+++ cheating_player.py
@@ -1,7 +1,9 @@
 """A smart cheating Hanabi player.
 
-Cheating player which tries to make intelligent moves when looking at his own cards.
-The following table gives the approximate percentages of this strategy reaching maximum score (using 6 suits).
+Tries to make intelligent moves when looking at his own cards.  The following
+table gives the approximate percentages of this strategy reaching maximum score
+(using 6 suits).
+
 Players | %
 --------+----
    2    | 90
@@ -10,35 +12,41 @@ Players | %
    5    | 97
 
 Possible improvements:
-- When playing a card, prefer to play a card you don't see in someone else's hand
-- When playing a card, prefer to play a card which allow other people to play cards of the same suit
-- When discarding a card you see in someone else's hand: don't do this if the other play has a lot of playable cards
-- When discarding the first card with some name, prefer to discard a card of a suit with low progress.
+- When playing a card, prefer one you don't see in someone else's hand
+- When playing a card, prefer one that allows other players to follow up in the
+  same suit
+- When discarding a card you see in someone else's hand: don't do this if the
+  other player has a lot of playable cards
+- When discarding the first copy of a card, prefer suits with low progress
 """
 
 from hanabi_classes import *
 from bot_utils import get_plays
 
 def get_played_cards(cards, progress):
-    """Return a list of cards which are already played; call only on visible cards!"""
-    return filter(lambda x: int (x['name'][0]) <= progress[x['name'][1]], cards)
+    """Return a list of already played cards; call only on visible cards!"""
+    return list(filter\
+            (lambda x: int (x['name'][0]) <= progress[x['name'][1]], cards))
 
 def get_duplicate_cards(cards):
-    """Returns a list of duplicates in cards; call only on visible cards!"""
-    names = map(lambda x: x['name'],cards)
-    return filter(lambda x: names.count(x['name']) > 1, cards)
+    """Return a list of duplicates in cards; call only on visible cards!"""
+    names = list(map(lambda x: x['name'], cards))
+    return list(filter(lambda x: names.count(x['name']) > 1, cards))
 
 def get_visible_cards(cards1, cards2):
-    """Returns a list of cards in cards1 which also occurs in cards2; call only on visible cards!"""
-    names2 = map(lambda x: x['name'],cards2)
-    return filter(lambda x: x['name'] in names2, cards1)
+    """Return a list of the intersection of cards1 and cards2; call only on
+    visible cards!"""
+    names2 = map(lambda x: x['name'], cards2)
+    return list(filter(lambda x: x['name'] in names2, cards1))
 
 def get_nonvisible_cards(cards1, names2):
-    """Returns a list of cards in cards1 which have a name not occuring in names2; call only on visible cards!"""
-    return filter(lambda x: x['name'] not in names2, cards1)
+    """Return a list of cards that are in cards1 but not names2; call only on
+    visible cards!"""
+    return list(filter(lambda x: x['name'] not in names2, cards1))
 
 def find_highest(cards):
-    """Returns list of cards with highest value in a list; call only on visible cards!"""
+    """Return a list of cards from the input that have the highest value; call
+    only on visible cards!"""
     highest = 0
     l = []
     for c in cards:
@@ -51,8 +59,8 @@ def find_highest(cards):
     return l
 
 def find_lowest(cards):
-    """Returns list of cards with lowest value in a list; call only on visible cards!"""
-    lowest = 20
+    """Analogous to find_highest"""
+    lowest = 20 # Arbitrary large number
     l = []
     for c in cards:
         value = int(c['name'][0])
@@ -64,19 +72,20 @@ def find_lowest(cards):
     return l
 
 def other_players(r, me):
-    """Returns a list of all players except me, in turn order starting after me"""
+    """Return a list of all players but me, in turn order starting after me"""
     return list(range(me+1, r.nPlayers)) + list(range(0, me))
 
 def get_all_visible_cards(player, r):
-    """Returns list of visible cards for player"""
+    """Return a list of the cards that player can see"""
     l = []
     for i in other_players(r, player):
         l.extend(r.h[i].cards)
     return l
 
 def count_unplayed_cards(r, progress):
-    """Returns the number of cards which are not yet played
-    (TODO: exclude cards which are unplayable because they or a lower number are discarded)"""
+    """Return the number of cards which are not yet played
+    (TODO: exclude cards which are unplayable because they or a lower number are
+    discarded)"""
     count = 0
     for suit in r.suits:
         count += 5 - progress[suit]
@@ -86,10 +95,12 @@ def count_unplayed_cards(r, progress):
 class CheatingPlayer:
 
     def want_to_discard(self, cards, player, r, progress):
-        """Returns a list (badness, card) where i is a number indicating how bad it is to discard one of the cards for player and card is a card which is least bad to discard.
+        """Returns a list (badness, card) where badness indicates how bad it is
+        to discard one of the cards for player and card is the least bad option.
         badness = 0: discard useless card
         badness = 1: discard card which is already in some else's hand
-        badness between 10 and 30: discard the first copy of a non-5 card (4s are badness 10, 3s badness 20, 2s badness 30)
+        badness between 10 and 30: discard the first copy of a non-5 card (4s
+        are badness 10, 3s badness 20, 2s badness 30)
         badness >= 100: discard a necessary card"""
         discardCards = get_played_cards(cards, progress)
         if discardCards: # discard a card which is already played
@@ -97,12 +108,12 @@ class CheatingPlayer:
         discardCards = get_duplicate_cards(cards)
         if discardCards: # discard a card which occurs twice in your hand
             return [0, random.choice(discardCards)]
-        discardCards = get_visible_cards(cards, get_all_visible_cards(player, r))
+        discardCards = get_visible_cards(cards, get_all_visible_cards(player,r))
         if discardCards: # discard a card which you can see (lowest first)
             discardCards = find_lowest(discardCards)
             return [1, random.choice(discardCards)]
         discardCards = get_nonvisible_cards(cards, r.discardpile)
-        discardCards = filter(lambda x: x['name'][0] != '5', discardCards)
+        discardCards = list(filter(lambda x: x['name'][0] != '5', discardCards))
         if discardCards: # discard a card which is not unsafe to discard
             discardCards = find_highest(discardCards)
             card = random.choice(discardCards)
@@ -122,10 +133,9 @@ class CheatingPlayer:
             wanttoplay = find_lowest(playableCards)
             return 'play', random.choice(wanttoplay)
 
-        # Hint if you are at maximum hints and cannot play
-        if r.hints == 8:
+        if r.hints == 8: # Hint if you are at maximum hints and cannot play.
             return 'hint', (nextplayer, '5')
-        # don't discard in endgame if someone else can play
+        # In endgame, don't discard if someone else can play.
         someone_can_play = False
         for i in other_players(r, me)[0:r.hints]:
             if get_plays(r.h[i].cards, progress):
@@ -134,8 +144,12 @@ class CheatingPlayer:
             assert r.hints > 0
             return 'hint', (nextplayer, '4')
         badness, discard = self.want_to_discard(cards, me, r, progress)
-        # discard if you can safely discard or if you have no hints
-            # TODO: increase discard badness if you already have a lot of unique cards (cards which still needs playing, and where all other copies are already discarded) in your hand
+        # Discard if you can safely discard or if you have no hints.
+        #
+        # TODO: increase discard badness if you already have a lot of unique
+        # cards (cards which still needs playing, and where all other copies are
+        # already discarded) in your hand
+        #
         if badness < 10 or r.hints == 0:
             return 'discard', discard
         other_badness = []
@@ -143,9 +157,13 @@ class CheatingPlayer:
             if get_plays(r.h[i].cards, progress):
                 other_badness.append(0)
             else:
-                other_badness.append(self.want_to_discard(r.h[i].cards, i, r, progress)[0])
+                other_badness.append(self.want_to_discard\
+                        (r.h[i].cards, i, r, progress)[0])
         other_badness = other_badness
-        # if someone can play or discard more safely before you run out of hints, give a hint
+        #
+        # If someone can play or discard more safely before you run out of
+        # hints, give a hint.
+        #
         if min(other_badness) < badness:
             return 'hint', (nextplayer, '3')
         return 'discard', discard
