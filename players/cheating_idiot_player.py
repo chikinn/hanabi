47d4fe86207ef191e2a8d99229d108ae85c42ac7
sean9keenan <sean9keenan@gmail.com>
2016-07-24 18:14:26 -0700

    Improved example in cheating_idiot_player

--- players/cheating_idiot_player.py
+++ players/cheating_idiot_player.py
@@ -13,6 +13,10 @@ class CheatingIdiotPlayer(AIPlayer):
     def get_name(cls):
         return 'idiot'
 
+    def __init__(self, *args):
+    	"""Can be overridden to perform initialization, but must call super"""
+        super(CheatingIdiotPlayer, self).__init__(*args)
+
     def play(self, r):
         cards = r.h[r.whoseTurn].cards
         progress = r.progress
@@ -22,3 +26,7 @@ class CheatingIdiotPlayer(AIPlayer):
             return 'discard', random.choice(cards)
         else:
             return 'play', random.choice(playableCards)
+
+    def end_game_logging(self):
+    	"""Can be overridden to perform logging at the end of the game"""
+    	pass
